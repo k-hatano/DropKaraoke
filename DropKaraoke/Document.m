fd89a3b885086b0ea084862d48a7529232afdce2
kenta.hatano <kenta.hatano@s-cubism.jp>
2017-07-04 15:30:54 +0900

    試しにコミット

--- DropKaraoke/Document.m
+++ DropKaraoke/Document.m
@@ -8,6 +8,8 @@
 
 #import "Document.h"
 
+#include "WavUtil.h"
+
 @interface Document ()
 
 @end
@@ -41,11 +43,59 @@
 }
 
 
-- (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError {
-    // Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning NO.
-    // You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
-    // If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
-    [NSException raise:@"UnimplementedMethod" format:@"%@ is unimplemented", NSStringFromSelector(_cmd)];
+- (BOOL)readFromURL:(NSURL *)url ofType:(NSString *)typeName error:(NSError * _Nullable __autoreleasing *)outError {
+    NSString *path = [url path];
+    NSLog(@"%@", path);
+    
+    StereoWAV inputWav;
+    MonoWAV outputWav;
+    unsigned int i;
+    
+    const char *importFileName;
+    char *exportFileName;
+    
+    importFileName = [path cStringUsingEncoding:NSASCIIStringEncoding];
+    
+    if (strstr(importFileName, ".wav") == NULL && strstr(importFileName, ".WAV") == NULL) {
+        printf("Error: import file seems not to be a wav file \n");
+        return 0;
+    }
+    
+    exportFileName = calloc(strlen(importFileName) + 4, sizeof(char));
+    strcpy(exportFileName, importFileName);
+    strcpy(&exportFileName[strlen(importFileName) - 4], "_vk.wav");
+    
+    printf("Importing wav file...\n");
+    int result = importStereoWav(&inputWav, importFileName);
+    
+    if (result == ERROR_FILE_NOT_FOUND) {
+        printf("Error: input file not found.\n");
+    } else if (inputWav.channels != 2) {
+        printf("Error: input file is not a stereo wav file.\n");
+    } else {
+        outputWav.sampleRate = inputWav.sampleRate;
+        outputWav.bps = inputWav.bps;
+        outputWav.length = inputWav.length;
+        
+        outputWav.data = calloc(outputWav.length, sizeof(double));
+        
+        printf("Generating wave data...\n");
+        for (i = 0; i < outputWav.length; i++) {
+            outputWav.data[i] = inputWav.dataLeft[i] - inputWav.dataRight[i];
+        }
+        
+        printf("Exporting wav file...\n");
+        exportMonoWav(&outputWav, exportFileName);
+        
+        printf("Complete!\n");
+        
+        free(outputWav.data);
+    }
+    
+    free(inputWav.dataLeft);
+    free(inputWav.dataRight);
+    free(exportFileName);
+    
     return YES;
 }
 
